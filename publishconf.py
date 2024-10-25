510e18d89b015553633edf013243652d1887a54c
M. Shahe Ansar <shaheansar@protonmail.com>
2024-10-25 06:56:00 -0400

    Trivial fix

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://shaheansar.github.io/Blog"
+SITEURL = "https://shaheansar.github.com/Blog"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
